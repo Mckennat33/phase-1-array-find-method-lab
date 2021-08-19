25c7e1b0a564388a0c586cb17da8b0009a5773c4
Thomas <thomasmckenna12gmail.com>
2021-08-19 13:50:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 
+ function superbowlWin(array) {
+     array.result === "W"
+     return array.year;
+     }
+    
+     record.find(superbowlWin)
+
+
+     
+     // let x = record.find(superbowlWin)
+ 
+      // record.filter(superbowlWin);
+
+   //  var x = record.find(x => x.result === "W")
+    
+// const winner = superbowlWin();   
+
+ // record.find(x => x.result === "W")
+
+
+// item.find(array => {
+//     if (array.result === "W") {
+//     else
+// }
+// }) 
+
